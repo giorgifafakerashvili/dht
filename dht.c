62db2ff06af33f978e0629e37ecb06b7329fd1d8
= <giorgi.fafa@gmail.com>
2017-02-24 02:35:22 +0400

    modified

--- dht.c
+++ dht.c
@@ -508,3 +508,201 @@ previous_bucket(struct bucket* b) {
 	}
 }
 
+/* Every bucket contains an unordered list of nodes */ 
+static struct node* 
+find_node(const unsigned char* id, int af) {
+	struct bucket* b = find_bucket(id, af); 
+	struct node* n; 
+	if(b == NULL) 
+		return NULL; 
+
+	n = b->nodes; 
+	while(n) {
+		if(id_cmp(n->id, id) == 0) 
+			return n; 
+		n = n->next; 
+	} 
+
+	return NULL; 
+}
+
+/* Returns a random node in a bucket */ 
+static struct node* 
+random_node(struct bucket* b) {
+	struct node* n; 
+	int nn; 
+
+	if(b->count == 0) return NULL; 
+
+	nn = random() % b->count; 
+	n = b->nodes; 
+
+	while(nn > 0 && n) {
+		n = n->next; 
+		nn--; 
+	} 
+
+	return n; 
+}
+
+/* Returns the middle id of bucket */ 
+static int 
+bucket_middle(struct bucket* b, unsigned char* id_return) {
+	int bit1 = lowbit(b->first); 
+	int bit2 = b->next  ? lowbit(b->next->first) : -1; 
+
+	if(bit >= 160) 
+		return -1; 
+
+	memcpy(id_return, b->first, 20); 
+	id_return[bit/8] = (0x80 >> (bit % 8)); 
+
+	return 1; 
+}
+
+/* Returns a random id within a bucket */ 
+static int 
+bucket_random(struct bucket* b, unsigned char* id_return) {
+	int bit1 = lowbit(b->first); 
+	int bit2 = b->next ? lowbit(b->next->first0) : -1; 
+	int bit = MAX(bit1, bit2) + 1; 
+	int i; 
+
+	if(bit >= 160) {
+		memcpy(id_return, b->first, 20); 
+		return 1; 
+	} 
+
+	memcpy(id_return, b->first, bit / 8); 
+	id_return[bit/8] = b->first[bit/8] & (0xFF00 >> (bit % 8)); 
+	id_return[bit/8] |= random() & 0xFF >> (bit % 8); 
+	for(i = bit / 8 + 1; i < 20; ++i) {
+		id_return[i] = random() & 0xFF; 
+	} 
+
+	return 1; 
+}
+
+/* INsert a new node into a bucket */ 
+static struct node* 
+insert_node(struct node* node) {
+	struct bucket* b = find_bucket(node->id, node->ss.ss_family); 
+
+	if(b == NULL) 
+		return NULL; 
+
+	node->next = b->nodes; 
+	b->nodes = node; 
+	b->count++; 
+
+	return node; 
+}
+
+/* This is our definition of a known-good node */ 
+static int 
+node_good(struct node* node) {
+	return node->pinged <= 2 && 
+		node->reply_time >= now.tv_sec -7200 && 
+		node->time >= now.tv_sec - 900; 
+}
+
+/* Our transaction-ids are 4-bytes long, with the first two bites 
+ * identifying the kind of request, and the remaining tow a sequence number 
+ * in host order */ 
+static void 
+make_tid(unsigned char* tid_return, const char* prefix, unsigned short seqno) {
+	tid_return[0] = prefix[0] & 0xFF; 
+	tid_return[1] = prefix[1] & 0xFF; 
+	memcpy(tid_return + 2, &seqno, 2); 
+}
+
+static int 
+tid_match(const unsigned char* tid, const char* prefix, 
+		  unsigned short* seqno_return) {
+	if(tid[0] == (prefix[0] & 0xFF) && tid[1] == (prefix[1]&0xFF)) {
+		if(seqno_return)
+			memcpy(seqno_return, tid + 2, 2); 
+		return 1; 
+	} else
+		return 0;
+}
+
+/* Every bucket caches the address of likely node. Ping it */ 
+static int 
+send_cached_ping(struct bucket* b) {
+	unsigned char tid[4]; 
+	int rc; 
+	/* We set familiy to 0 when there's no cached node */ 
+	if(b->cached.ss_family == 0) 
+		return 0; 
+
+	debugf("Sending ping to cached node\n"); 
+	make_tid(tid, "pn",0); 
+	rc = send_ping((struct sockaddr*)&b->cached, b->cachedlen, tid, 4); 
+	b->cached.ss_family = 0; 
+	b->cachedlen = 0; 
+	return rc; 
+}
+
+/* Called whenever we send a request to a node, increases the ping count 
+ * and, if tha reaches 3, send a ping to a new candiadate */ 
+static void 
+ping(struct node* n, struct bucket* b) {
+	n->pinged++; 
+	n->pinged_time = now.tv_sec; 
+	if(n->pinged >= 3) 
+		send_cached_ping(b ? b : find_bucket(n->id, n->ss.ss_family)); 
+}
+
+
+/* The internal blacklist is a LRU cache of nodes that have sent 
+ * incorrect messages */ 
+static void 
+blacklist_node(const unsigned char* id, const struct sockaddr* sa, int salen) {
+	int i; 
+
+	debugf("Blacklist broken node \n"); 
+
+	if(id) {
+		struct node* n; 
+		struct search* sr; 
+		/* Make the node easy to discard */ 
+		n =find_node(id, sa->sa_family); 
+		if(n) {
+			n->pinged = 3; 
+			pinged(n, NULL); 
+		}
+
+		/* discard it from search in progress */ 
+		sr = searches; 
+		while(sr) {
+			for(i = 0; i < sr->numnodes; ++i) {
+				if(id_cmp(sr->nodes[i].id, id) == 0) 
+					flush_search_node(&sr->nodes[i], sr); 
+			}
+			sr = sr->next; 
+		}
+	}
+	/* And make sure we dont' hear from it again */ 
+	memcpy(&blacklist[next_blacklisted], sa, salen); 
+	next_blacklisted = (next_blacklisted + 1) % DHT_MAX_BLACKLISTED; 
+}
+
+static int 
+node_blacklisted(const struct sockaddr* sa, int salen) {
+	int i; 
+
+	if((unsigned) salen > sizeof(struct sockaddr_storage)) 
+		abort(); 
+
+	if(dht_blacklisted(sa, salen)) 
+		return 1; 
+
+	for(i = 0; i < DHT_MAX_BLACKLISTED; ++i) {
+		if(memcmp(&blacklisted[i], sa, salen) == 0) 
+			return 1; 
+	}
+
+	return 0; 
+}
+
